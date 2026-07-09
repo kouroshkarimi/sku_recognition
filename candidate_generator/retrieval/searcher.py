30e4732fc4d8b1592f5f2c8b50569ad4e367d0c2
kouroshkarimi <thekouroshkarimi@gmail.com>
2026-07-09 15:56:11 -0400

    fix(candidate_generator/retrieval/searcher.py): update the code by pylint instructions

--- candidate_generator/retrieval/searcher.py
+++ candidate_generator/retrieval/searcher.py
@@ -1,3 +1,6 @@
+'''
+A class for searching similar embeddings in the FAISS index.
+'''
 from pathlib import Path
 
 from PIL import Image
@@ -7,7 +10,13 @@ from candidate_generator.retrieval.faiss_index import FaissIndex
 
 
 class Searcher:
-
+    '''
+    A class for searching similar embeddings in the FAISS index.
+    Attributes:
+        extractor (EmbeddingExtractor): An instance of the
+          EmbeddingExtractor class for encoding images.
+        index (FaissIndex): An instance of the FaissIndex class for searching embeddings.
+    '''
     def __init__(
         self,
         extractor: EmbeddingExtractor,
@@ -27,7 +36,15 @@ class Searcher:
         image_path: Path,
         top_k: int = 5,
     ):
-
+        '''
+        Search for the top-k most similar embeddings to the provided image.
+        Args:
+            image_path (Path): The path to the query image.
+            top_k (int): The number of top similar embeddings to retrieve.
+        Returns:
+            scores (np.ndarray): The similarity scores of the top-k embeddings.
+            indices (np.ndarray): The indices of the top-k embeddings in the index.
+        '''
         image = Image.open(image_path).convert("RGB")
 
         embedding = self.extractor.encode(image)
