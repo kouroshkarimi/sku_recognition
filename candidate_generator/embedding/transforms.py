659d868f762403cbda761b8f2c30042d540e7c15
kouroshkarimi <thekouroshkarimi@gmail.com>
2026-07-09 15:47:39 -0400

    fix(candidate_generator/embedding/transforms.py): update the code by pylint instructions

--- candidate_generator/embedding/transforms.py
+++ candidate_generator/embedding/transforms.py
@@ -1,7 +1,23 @@
+'''
+This module defines the build_transform function,
+which constructs a series of image transformations
+to preprocess images for embedding extraction using
+the DINOv2 model. The transformations include resizing,
+normalization, and conversion to tensor format,
+ensuring that the input images are in the appropriate format
+for the model.
+'''
 from torchvision import transforms
 
 
 def build_transform(image_size: int = 518):
+    '''
+    Build a series of image transformations for preprocessing.
+    Args:
+        image_size (int): The size to which the image will be resized.
+    Returns:
+        transforms.Compose: A composition of image transformations.
+    '''
     return transforms.Compose(
         [
             transforms.Resize(
