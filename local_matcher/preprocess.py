00797d504f584fe80416ecf664c0bafd46f9aa76
kouroshkarimi <thekouroshkarimi@gmail.com>
2026-07-09 15:00:14 -0400

    fix(preprocess.py): update the code by pylint instructions

--- local_matcher/preprocess.py
+++ local_matcher/preprocess.py
@@ -1,3 +1,8 @@
+'''
+This module is responsible for some preprocessing steps for the GIMMatcher class.
+It provides functions to read an image from a file, preprocess the image by resizing and normalizing
+it.
+'''
 import numpy as np
 import torch
 import torchvision.transforms.functional as F
@@ -6,6 +11,15 @@ import cv2
 
 
 def read_image(path, grayscale=False):
+    '''
+    Read an image from a file and return it as a numpy array.
+    If grayscale is True, the image is read in grayscale mode.
+    Args:
+        path (str): The path to the image file.
+        grayscale (bool): Whether to read the image in grayscale mode. Default is False.
+    Returns:
+        np.ndarray: The image as a numpy array.
+    '''
     if grayscale:
         mode = cv2.IMREAD_GRAYSCALE
     else:
@@ -20,6 +34,16 @@ def read_image(path, grayscale=False):
 
 def preprocess(image: np.ndarray, grayscale: bool = False, resize_max: int = None,
                dfactor: int = 8):
+    '''
+    Preprocess the image by resizing and normalizing it.
+    Args:
+        image (np.ndarray): The image to preprocess.
+        grayscale (bool): Whether to convert the image to grayscale. Default is False.
+        resize_max (int): The maximum size to resize the image to. Default is None.
+        dfactor (int): The factor by which the image size should be divisible. Default is 8.
+    Returns:
+        tuple: The preprocessed image and the scale factor.
+    '''
     image = image.astype(np.float32, copy=False)
     size = image.shape[:2][::-1]
     scale = np.array([1.0, 1.0])
@@ -50,6 +74,15 @@ def preprocess(image: np.ndarray, grayscale: bool = False, resize_max: int = Non
 
 
 def resize_image(image, size, interp):
+    '''
+    Resize the image to the specified size using the specified interpolation method.
+    Args:
+        image (np.ndarray): The image to resize.
+        size (tuple): The size to resize the image to.
+        interp (str): The interpolation method to use. Must start with 'cv2_'.
+        Returns:
+            np.ndarray: The resized image.
+    '''
     assert interp.startswith('cv2_')
     if interp.startswith('cv2_'):
         interp = getattr(cv2, 'INTER_'+interp[len('cv2_'):].upper())
