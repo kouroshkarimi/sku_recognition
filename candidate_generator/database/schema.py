4ac11cd783eb6ab5ee0601c32b9ecb8a0c1da00c
kouroshkarimi <thekouroshkarimi@gmail.com>
2026-07-09 15:30:49 -0400

    fix(candidate_generator/database/schema.py): update the code by pylint instructions

--- candidate_generator/database/schema.py
+++ candidate_generator/database/schema.py
@@ -1,16 +1,33 @@
-from pathlib import Path
+'''
+This module defines the SchemaManager class, which is responsible for creating
+the database schema for the SKU recognition system. It provides methods to create
+the necessary tables for storing SKU and gallery image information.
+The schema includes two tables: "sku" for storing SKU information and "image"
+for storing gallery image information. The "image" table has a foreign key
+relationship with the "sku" table, linking each gallery image to its corresponding SKU.
+'''
 
+from pathlib import Path
 from .database import Database
 
-
 class SchemaManager:
-
+    '''
+    SchemaManager is a class that is responsible for creating the database schema
+    '''
     def __init__(self, database_path: Path):
 
         self.database = Database(database_path)
 
     def create(self):
-
+        '''
+        Creates the database schema by creating the necessary tables for storing
+        SKU and gallery image information. The schema includes two tables: "sku" for
+        storing SKU information and "image" for storing gallery image information.
+        Args:
+            None
+        Returns:
+            None
+    '''
         connection = self.database.connect()
 
         cursor = connection.cursor()
@@ -56,5 +73,4 @@ class SchemaManager:
         """)
 
         connection.commit()
-
         connection.close()
