eccde90ce7a0733d24998fbc69d44591c35c20af
kouroshkarimi <thekouroshkarimi@gmail.com>
2026-07-09 15:23:36 -0400

    fix(candidate_generator/database/database.py): update the code by pylint instructions

--- candidate_generator/database/database.py
+++ candidate_generator/database/database.py
@@ -1,9 +1,20 @@
+'''
+This module defines the Database class, which provides methods for
+ connecting to a SQLite database.
+The Database class is initialized with a database path
+and provides a method to connect to the database.
+The connect method establishes a connection to
+the SQLite database and enables foreign key support.
+'''
+
 from pathlib import Path
 import sqlite3
 
 
 class Database:
-
+    '''
+    Database is a class that provides methods for connecting to a SQLite database.
+    '''
     def __init__(self, database_path: Path):
 
         self.database_path = database_path
@@ -14,7 +25,11 @@ class Database:
         )
 
     def connect(self) -> sqlite3.Connection:
-
+        '''
+        Connect to the SQLite database and enable foreign key support.
+        Returns:
+            sqlite3.Connection: A connection object to the SQLite database.
+        '''
         connection = sqlite3.connect(self.database_path)
 
         connection.execute(
